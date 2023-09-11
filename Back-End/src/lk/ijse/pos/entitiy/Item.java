313db08747844a79ddc8b9b177e741a585a43397
Gayani Dewindi Vitharana <gayanidewindi@gmail.com>
2023-09-11 09:17:33 +0530

    layered added

---
+++ Back-End/src/lk/ijse/pos/entitiy/Item.java
@@ -0,0 +1,14 @@
+package lk.ijse.pos.entitiy;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Item {
+    private String code;
+    private String name;
+    private int qty;
+    private double price;
+}
